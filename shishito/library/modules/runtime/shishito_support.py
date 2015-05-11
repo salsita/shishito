83aec87b49e0c874770b1efe1e4ebe2699ba167d
Filip Bures <filipb@salsitasoft.com>
2015-05-11 17:25:03 +0200

    Refactor main runner, Reporter and ShishitoSupport

--- shishito/library/modules/runtime/shishito_support.py
+++ shishito/library/modules/runtime/shishito_support.py
@@ -7,84 +7,143 @@ import sys
 
 
 class ShishitoSupport(object):
-    def __init__(self):
-        # TODO this does not work without a runner
-        self.project_root = os.getcwd()
+
+    def __init__(self, cmd_args=None, project_root=None):
+        """ cmd_args added only if called from runner.
+        when called from tests cmd_args have to be added to pytest and taken from pytest.config
+        """
+
+        # parsed arguments from commad_line
+        self.args_config = {} if cmd_args is None else cmd_args
+
+        # called without cmd_args
+        # if True gid will use only pytest.config
+        # if False gid uses pytest.config, cmd_args and config files
+        self.used_in_test = True if cmd_args is None else False
+
+        if project_root is not None:
+            self.project_root = project_root
+        else:
+            self.project_root = os.getcwd()
+
         self.configs = self.load_configs()
-        self.config = ConfigParser.RawConfigParser()
+
+        self.test_environment = self.gid('test_environment')
+        self.test_platform = self.gid('test_platform')
 
     def load_configs(self):
         """ Loads variables from .properties configuration files,  check if project didn't contain such folder
         (for non selenium projects) """
+
         config_path = os.path.join(self.project_root, 'config')
         config = ConfigParser.ConfigParser()
-        if os.path.exists(config_path):
-            # load server config variables
-            server_config = os.path.join(config_path, 'server_config.properties')
-            config.read(server_config)
-            server_config_vars = dict(config.defaults())
-            # load local config variables
-            local_config = os.path.join(config_path, 'local_config.properties')
-            config.read(local_config)
-            local_config_vars = dict(config.defaults())
-            # load non selenium config variables
-            non_selenium_config = os.path.join(config_path, 'non_selenium_config.properties')
-            config.read(non_selenium_config)
-            non_selenium_config = dict(config.defaults())
-            return_configs = [server_config_vars, local_config_vars, non_selenium_config]
-            return return_configs
+        if not os.path.exists(config_path):
+            return None
+
+        # load server config variables
+        server_config = os.path.join(config_path, 'server_config.properties')
+        config.read(server_config)
+        server_config_vars = dict(config.defaults())
+
+        # load local config variables
+        local_config = os.path.join(config_path, 'local_config.properties')
+        config.read(local_config)
+        local_config_vars = dict(config.defaults())
+
+        # load non selenium config variables
+        non_selenium_config = os.path.join(config_path, 'non_selenium_config.properties')
+        config.read(non_selenium_config)
+        non_selenium_config = dict(config.defaults())
+        return_configs = [server_config_vars, local_config_vars, non_selenium_config]
+
+        return return_configs
 
     def gid(self, key):
         """ Gets value from config variables based on provided key.
          If local execution parameter is "True", function will try to search for parameter in local configuration file.
          If such parameter is not found or there is an error while reading the file, server (default) configuration
          file will be used instead. """
+
         if not self.configs:
             return None
 
         # first try to lookup pytest config
         try:
+            # TODO: find out why these options have '='
+            if self.used_in_test and key in ['test_platform', 'test_environment', 'environment_configuration']:
+                key += '='
+
             value = pytest.config.getoption(key)
             if value:
                 return value
-            else:
-                use_logs = True
         except (ValueError, AttributeError):
-            use_logs = True
-        # lookup value from config files
-        if use_logs:
-            server_config = self.configs[0]
-            local_config = self.configs[1]
-            configs = []
-            if local_config.get('local_execution').lower() == 'true':
-                configs.append((local_config, 'local config'))
-            configs.extend([(server_config, 'server config'), (os.environ, 'env variables')])
-            for idx, cfg in enumerate(configs):
-                if key in cfg[0] and cfg[0][key] != '':
-                    # if idx:
-                    # print "%s not found in %s, using value from %s" % (key, configs[0][1], cfg[1])
-                    return cfg[0][key]
-                    # print "%s not found in any config" % key
+            pass
 
-    def get_environment_config(self, platform_name, environment_name):
+        # look in cmd args config
+        value = self.args_config.get(key)
+        if value:
+            return value
+
+        # lookup value from config files
+        server_config = self.configs[0]
+        local_config = self.configs[1]
+
+        configs = []
+
+        # TOOD: this can be done in __init__ instead of each gid call
+        if local_config.get('local_execution').lower() == 'true':
+            configs.append((local_config, 'local config'))
+
+        configs.extend([(server_config, 'server config'), (os.environ, 'env variables')])
+        for cfg in configs:
+            if key in cfg[0] and cfg[0][key] != '':
+                return cfg[0][key]
+
+    def get_environment_config(self, platform_name=None, environment_name=None):
         """ gets config """
-        # TODO review the functionality (is it what we want?)
+
+        # TODO: review the functionality (is it what we want?)
+
+        if platform_name is None:
+            platform_name = self.gid('test_platform')
+
+        if environment_name is None:
+            environment_name = self.gid('test_environment')
+
         config = ConfigParser.ConfigParser()
         config_path = os.path.join(self.project_root, 'config', platform_name, environment_name + '.properties')
-        if config_path:
-            config.read(config_path)
-            return config
-        else:
+
+        if not config_path:
             sys.exit('Config file in location {0} was not found! Terminating test.'.format(config_path))
 
-    def get_modules(self, platform, environment):
+        config.read(config_path)
+        return config
+
+    def get_modules(self, platform=None, environment=None, module=None):
+        if platform is None:
+            platform = self.gid('test_platform')
+
+        if environment is None:
+            environment = self.gid('test_environment')
+
         platform_execution = 'shishito.library.modules.runtime.platform.' + platform + '.control_execution'
         platform_test = 'shishito.library.modules.runtime.platform.' + platform + '.control_test'
-        test_environment = 'shishito.library.modules.runtime.environment.' + environment + '.control_environment'
-        return {'platform_execution': getattr(import_module(platform_execution), 'ControlExecution'),
-                'platform_test': getattr(import_module(platform_test), 'ControlTest'),
-                'test_environment': getattr(import_module(test_environment), 'ControlEnvironment')}
+        test_environment = 'shishito.library.modules.runtime.environment.' + environment + '_control_environment'
+
+        if module == 'platform_execution':
+            return getattr(import_module(platform_execution), 'ControlExecution')
+
+        if module == 'platform_test':
+            return getattr(import_module(platform_test), 'ControlTest')
+
+        if module == 'test_environment':
+            return getattr(import_module(test_environment), 'ControlEnvironment')
+
+        return {
+            'platform_execution': getattr(import_module(platform_execution), 'ControlExecution'),
+            'platform_test': getattr(import_module(platform_test), 'ControlTest'),
+            'test_environment': getattr(import_module(test_environment), 'ControlEnvironment')
+        }
 
     def get_test_control(self):
-        platform_test = 'shishito.library.modules.runtime.platform.' + self.gid('test_platform') + '.control_test'
-        return getattr(import_module(platform_test), 'ControlTest')()
+        return self.get_modules(module='platform_test')()

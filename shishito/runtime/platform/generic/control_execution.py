589938a3c864f5803f62fec6d4544f3cf1f51aae
Filip Bures <filipb@salsitasoft.com>
2015-05-21 13:17:46 +0200

    Add generic platform for running non selenium tests

---
+++ shishito/runtime/platform/generic/control_execution.py
@@ -0,0 +1,19 @@
+from shishito.runtime.platform.shishito_execution import ShishitoExecution
+
+
+class ControlExecution(ShishitoExecution):
+    """ ControlExecution for mobile platform"""
+
+    def get_test_result_prefix(self, config_section):
+        """ Create string prefix for test results.
+
+        :param str config_section: section in platform/environment.properties config
+        :return: str with test result prefix
+        """
+
+        return 'generic'
+
+    def run_tests(self):
+        """ Trigger PyTest runner. Run PyTest for for generic tests. """
+
+        return self.trigger_pytest('generic')
--- shishito/runtime/platform/shishito_execution.py
+++ shishito/runtime/platform/shishito_execution.py
@@ -5,7 +5,6 @@
 
 import os
 import pytest
-import sys
 
 
 class ShishitoExecution(object):

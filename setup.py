e6e9866e11277b22beae0e75ecad99d5d6a5d1d8
Filip Bures <filipb@salsitasoft.com>
2015-05-21 17:53:51 +0200

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+
+long_description = """
+shishito
+========
+
+Shishito is module for web application and browser extension integration testing
+with Selenium Webdriver & Python. It runs tests using included libraries and
+generates nice test results output.
+"""
+
+
+setup(
+    name='shishito',
+    version='2.0',
+    url='https://github.com/salsita/shishito',
+    description='Python module for selenium webdriver test execution',
+    long_description=long_description,
+    author='Vojtech Burian',
+    author_email='vojtech.burian@gmail.com',
+    license='MIT',
+    platforms='any',
+    keywords=['testing', 'selenium', 'webdriver', 'browserstack', 'appium'],
+    packages=find_packages(),
+    package_data={
+        'shishito': ['reporting/resources/*.js', 'reporting/resources/*.css', 'reporting/resources/*.html']
+    },
+    install_requires=[
+        'selenium', 'pytest-xdist', 'pytest-instafail', 'pytest', 'UnittestZero',
+        'Jinja2', 'requests', 'Appium-Python-Client'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Testing",
+    ],
+)

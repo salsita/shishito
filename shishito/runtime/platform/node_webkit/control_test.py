c09353efff2d2b8f05c0990e0cdeab5b46107e2b
Juraj Harman <juraj@salsitasoft.com>
2015-06-16 16:07:16 +0200

    - added temporary screenshot on failure functionality due to issue: https://code.google.com/p/chromedriver/issues/detail?id=816

--- shishito/runtime/platform/node_webkit/control_test.py
+++ shishito/runtime/platform/node_webkit/control_test.py
@@ -3,8 +3,10 @@
  Various startup and termination procedures, helper functions etc.
  Not to be used for directly testing the system under test (must not contain Asserts etc.)
 """
-
+import os
+import re
 from shishito.runtime.platform.shishito_control_test import ShishitoControlTest
+import pyscreenshot as Screenshotter
 
 
 class ControlTest(ShishitoControlTest):
@@ -26,3 +28,24 @@ class ControlTest(ShishitoControlTest):
     def stop_browser(self):
         # closing browser (node-webkit application) once tests are finished
         self.driver.quit()
+
+    def stop_test(self, test_info):
+        """
+        !!!TEMPORARY METHOD!!! \n
+        To be executed after every test-case (test function). If test failed, function saves
+        screenshots created during test.
+
+        For more information see: https://code.google.com/p/chromedriver/issues/detail?id=816
+
+        :param test_info: information about test
+        :return:
+        """
+        if test_info.test_status not in ('passed', None):
+            # save screenshot in case test fails
+            screenshot_folder = os.path.join(self.shishito_support.project_root, 'screenshots')
+
+            if not os.path.exists(screenshot_folder):
+                os.makedirs(screenshot_folder)
+
+            file_name = re.sub('[^A-Za-z0-9_. ]+', '', test_info.test_name)
+            Screenshotter.grab_to_file(os.path.join(screenshot_folder, file_name + '.png'))

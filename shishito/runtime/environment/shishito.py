36cd616aba494c332b0baf68845c696af90b905d
Katarina Achbergerova <katarina@salsitasft.com>
2017-01-26 11:11:33 +0100

    QA | ignore certificate error - CHROME

--- shishito/runtime/environment/shishito.py
+++ shishito/runtime/environment/shishito.py
@@ -84,7 +84,7 @@ class ShishitoEnvironment(object):
 
         if browser_type == 'chrome':
             chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+            chrome_options.add_argument('--ignore-certificate-errors')
             capabilities.update(chrome_options.to_capabilities())
         elif browser_type == 'firefox':
             profile = webdriver.FirefoxProfile()

7532ba7617e99984a7d7cbd576121fb6512d3dea
Jan Kotrlik <janko@salsitasoft.com>
2017-05-04 14:29:08 +0200

    missing dash

--- scripts/deploy-to-qa-server.sh
+++ scripts/deploy-to-qa-server.sh
@@ -4,4 +4,4 @@ set -x
 
 tar cvzf "shishito-${CIRCLE_BRANCH}.tgz" * .[^.]*
 
-scp "shishito${CIRCLE_BRANCH}.tgz" root@qa.salsitasoft.com:/usr/share/nginx/html/qa/
+scp "shishito-${CIRCLE_BRANCH}.tgz" root@qa.salsitasoft.com:/usr/share/nginx/html/qa/

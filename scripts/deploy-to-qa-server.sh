feaabb82c302f254646aa53a2b8ab54634b75f5e
Jan Kotrlik <janko@salsitasoft.com>
2017-05-04 15:21:05 +0200

    saving time on build, making tarball smaller

--- scripts/deploy-to-qa-server.sh
+++ scripts/deploy-to-qa-server.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-tar cvzf "shishito-${CIRCLE_BRANCH}.tgz" * .[^.]*
+tar cvzf "shishito-${CIRCLE_BRANCH}.tgz" shishito/ requirements setup.py
 
 scp "shishito-${CIRCLE_BRANCH}.tgz" root@qa.salsitasoft.com:/usr/share/nginx/html/qa/

fe4dd38a0f00dbc668b7e30ab2aeeed464fba801
Pavel Pribyl <pavelp@salsitasoft.com>
2017-07-12 12:46:52 +0200

    QA|Fix problem with debug_events filename

--- shishito/reporting/junithtml.py
+++ shishito/reporting/junithtml.py
@@ -322,7 +322,7 @@ class LogHTML(object):
         name = re.sub('[^A-Za-z0-9_.]+', '_', name)
         browser_name = self.prefix.split(",")[0].replace('[', '').lower()
 
-        source = os.path.join(self.project_root, 'debug_events', name + '.json')
+        source = os.path.join(self.project_root, 'debug_events', browser_name + '_' + name + '.json')
         self.used_debug_events.append(source)
         log.append(html.h3('DebugEvent log'))
         log.append(html.a(name + '.json', href='debug_events/' + name + '.json'))
